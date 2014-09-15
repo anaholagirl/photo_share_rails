b8d09515d496257234b10eccb568992fafd3dd9e
Linda Gibson & Marina Neumann <student@epicodus.com>
2014-09-15 11:39:01 -0700

    Pass first capybara test for logging in.

---
+++ spec/features/signin_pages_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+  describe "the signin process" do
+    it "signs a user in who uses the right password" do
+      user = User.create(:email => 'user@example.com', :password => 'password', :password_confirmation => 'password')
+      visit '/login'
+      fill_in 'Email', :with => 'user@example.com'
+      fill_in 'Password', :with => 'password'
+      click_button 'Log In'
+      expect(page).to have_content 'Say Cheese'
+    end
+  end
