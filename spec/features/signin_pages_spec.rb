5fd3725dd0c7903399842baee0fe652f2acc0222
Linda Gibson & Marina Neumann <student@epicodus.com>
2014-09-15 11:50:50 -0700

    Pass a capybara test that allows users to sign up.

--- spec/features/signin_pages_spec.rb
+++ spec/features/signin_pages_spec.rb
@@ -18,4 +18,13 @@ require 'rails_helper'
       click_button 'Log In'
       expect(page).to have_content 'Log In'
     end
+
+    it "allows a user to sign up" do
+      visit '/signup'
+      fill_in 'Email', with: 'user@email.com'
+      fill_in 'Password', with: 'password'
+      fill_in 'Password confirmation', with: 'password'
+      click_button 'Sign Up'
+      expect(page).to have_content 'Say Cheese'
+    end
   end
