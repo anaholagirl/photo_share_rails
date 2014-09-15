d5c0aeb883595d6c8150725d8e2d98bb9ade085d
Linda Gibson & Marina Neumann <student@epicodus.com>
2014-09-15 11:41:56 -0700

    Pass capybara test that will fail to sign in a user.

--- spec/features/signin_pages_spec.rb
+++ spec/features/signin_pages_spec.rb
@@ -9,4 +9,13 @@ require 'rails_helper'
       click_button 'Log In'
       expect(page).to have_content 'Say Cheese'
     end
+
+    it "gives a user an error who uses the wrong password" do
+      visit '/login'
+      user = User.create(:email => 'user@example.com', :password => 'password', :password_confirmation => 'password')
+      fill_in 'Email', :with => 'user@example.com'
+      fill_in 'Password', :with => 'wrong'
+      click_button 'Log In'
+      expect(page).to have_content 'Log In'
+    end
   end
