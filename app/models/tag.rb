21acb3655478d34addce5e1c80739d086b90f2b8
Linda Gibson & Marina Neumann <student@epicodus.com>
2014-09-16 15:48:18 -0700

    Establish uniqueness of tags.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ActiveRecord::Base
   validates :photo_id, :presence => true
-  validates :user_id, :presence => true
+  validates :user_id, :presence => true, :uniqueness => { :scope => :photo_id }
 
 
   belongs_to :photo
